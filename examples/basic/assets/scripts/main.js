4ba1a54d013133f7e7e057915d58ce7c74671d29
Arne Roomann-Kurrik <kurrik@gmail.com>
2015-04-05 21:37:11 -0700

    Script more

--- examples/basic/assets/scripts/main.js
+++ examples/basic/assets/scripts/main.js
@@ -2,4 +2,5 @@ console.log('Woop');
 addEventListener('foo', function (player) {
   var pos = player.Pos();
   console.log("Got 'foo' event! Player X:", pos.X, "Player Y:", pos.Y);
+  player.MoveToCoords(pos.X + 1, pos.Y + 1);
 });
